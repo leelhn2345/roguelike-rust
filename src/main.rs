606e9348526463c1dc3c9d8801921c5f0889d1d5
Nelson Lee <leelhn2345@outlook.com>
2023-02-01 17:51:03 +0800

    01 - graphics

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,17 @@
 use tcod::colors::*;
 use tcod::console::*;
 
+// actual size of the window
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
-const LIMIT_FPS: i32 = 20;
+const LIMIT_FPS: i32 = 20; // 20 frames-per-second maximum
 
-// encapsulate all of `libtcod-related` values into a single Rust `struct`
-// easier to pass it around to functions
 struct Tcod {
     root: Root,
 }
 
-fn main() {
-    // println!("Hello, world!");
-
-    let root = Root::initializer()
-        .font("arial10x10.png", FontLayout::Tcod)
-        .font_type(FontType::Greyscale)
-        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
-        .title("Rust/libtcod tutorial")
-        .init();
-
-    let mut tcod = Tcod { root };
-
-    tcod::system::set_fps(LIMIT_FPS);
-
-    let mut player_x = SCREEN_WIDTH / 2;
-    let mut player_y = SCREEN_HEIGHT / 2;
-
-    while !tcod.root.window_closed() {
-        tcod.root.set_default_foreground(WHITE);
-        tcod.root.clear();
-        tcod.root.put_char(1, 1, '@', BackgroundFlag::None);
-        tcod.root.flush();
-        tcod.root.wait_for_keypress(true);
-
-        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
-        if exit {
-            break;
-        }
-    }
-}
-
 fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool {
-    // todo: handle keys
     use tcod::input::Key;
     use tcod::input::KeyCode::*;
 
@@ -69,5 +36,36 @@ fn handle_keys(tcod: &mut Tcod, player_x: &mut i32, player_y: &mut i32) -> bool 
 
         _ => {}
     }
+
     false
+}
+
+fn main() {
+    tcod::system::set_fps(LIMIT_FPS);
+
+    let root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();
+
+    let mut tcod = Tcod { root };
+
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
+    while !tcod.root.window_closed() {
+        tcod.root.set_default_foreground(WHITE);
+        tcod.root.clear();
+        tcod.root
+            .put_char(player_x, player_y, '@', BackgroundFlag::None);
+        tcod.root.flush();
+
+        // handle keys and exit game if needed
+        let exit = handle_keys(&mut tcod, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
+    }
 }
